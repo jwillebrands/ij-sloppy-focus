6b1306c3bd362ed4dad58050bf032262c0b56063
Jan-Willem Willebrands <jan-willem@willebrands.dev>
2023-06-02 16:04:21 +0200

    feat: improve active popup detection

--- src/main/kotlin/dev/willebrands/intellij/sloppyfocus/veto/PopupMenuStateVetoableFocusListener.kt
+++ src/main/kotlin/dev/willebrands/intellij/sloppyfocus/veto/PopupMenuStateVetoableFocusListener.kt
@@ -2,10 +2,17 @@ package dev.willebrands.intellij.sloppyfocus.veto
 
 import com.intellij.openapi.actionSystem.ex.ActionManagerEx
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.popup.JBPopupFactory
 import java.awt.Component
 
 class PopupMenuStateVetoableFocusListener : VetoableFocusSwitchListener {
     override fun allowFocusSwitch(target: Component, project: Project): Boolean {
-        return ActionManagerEx.getInstanceEx().isActionPopupStackEmpty
+        val actionPopupStackEmpty = ActionManagerEx.getInstanceEx().isActionPopupStackEmpty
+        val popupActive = JBPopupFactory.getInstance().isPopupActive
+        val allowFocusSwitch = actionPopupStackEmpty && !popupActive
+        logVetoDecision(allowFocusSwitch) {
+            "Action popup stack empty = $actionPopupStackEmpty; popup active = $popupActive"
+        }
+        return allowFocusSwitch
     }
 }
