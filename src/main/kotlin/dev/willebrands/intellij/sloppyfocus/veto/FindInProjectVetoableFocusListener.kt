fa60f72458780b5b2695e2202906f7afef598127
Jan-Willem Willebrands <jan-willem@willebrands.dev>
2024-10-14 10:39:09 +0200

    fix: replace use of 2024.3 internal API
    FindPopupPanel has been changed to internal in 2024.3. The FindUI interface
    it derives from currently is not, so seems a reasonable alternative.

--- src/main/kotlin/dev/willebrands/intellij/sloppyfocus/veto/FindInProjectVetoableFocusListener.kt
+++ src/main/kotlin/dev/willebrands/intellij/sloppyfocus/veto/FindInProjectVetoableFocusListener.kt
@@ -1,6 +1,6 @@
 package dev.willebrands.intellij.sloppyfocus.veto
 
-import com.intellij.find.impl.FindPopupPanel
+import com.intellij.find.impl.FindUI
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.IdeFocusManager
 import com.intellij.util.ui.UIUtil
@@ -9,14 +9,19 @@ import java.awt.Component
 class FindInProjectVetoableFocusListener : VetoableFocusSwitchListener {
     override fun allowFocusSwitch(target: Component, project: Project): Boolean {
         /**
-         * Unfortunately, I could not a Find/Replace related Manager we can ask about the state of the modal.
-         * Therefor we resort to checking whether the current focus owner is (a child of) the FindPopupPanel.
+         * Unfortunately, I could not find a Find/Replace related Manager we can ask about the state of the modal.
+         * Therefor we resort to checking whether the current focus owner is (a child of) a FindUI component.
          * If it is, the focus switch is vetoed.
          */
         val focusOwner = IdeFocusManager.getInstance(project).focusOwner
-        val parentFindPopupPanel = UIUtil.getParentOfType(FindPopupPanel::class.java, focusOwner)
-        val isChildOfFindPopupPanel = parentFindPopupPanel != null
-        val targetIsChildOfFindPopupPanel = parentFindPopupPanel?.let { UIUtil.isDescendingFrom(target, it) } ?: false
-        return decideAndLog(!isChildOfFindPopupPanel || targetIsChildOfFindPopupPanel) { "Current focused component is (child of) FindPopupPanel" }
+        val parentFindUI = UIUtil.getParentOfType(FindUI::class.java, focusOwner)
+        val isChildOfFindUI = parentFindUI != null
+        val targetIsChildOfFindPopupPanel = parentFindUI
+            ?.let { it as? Component }
+            ?.let { UIUtil.isDescendingFrom(target, it) } ?: false
+        return decideAndLog(!isChildOfFindUI || targetIsChildOfFindPopupPanel) {
+            "Focus owner is child of findUI: $isChildOfFindUI; " +
+                    "target is child of findUI: $targetIsChildOfFindPopupPanel"
+        }
     }
 }
